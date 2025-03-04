176a286c27d4e71bbb3c9967c4c8bf65f1f7dd27
Vignesh Gawali <vbg3008@gmail.com>
2025-03-04 20:26:29 +0530

    adding some animation

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let opener = document.querySelector("#opener");
 let links = document.querySelector("#links");
 
-
 opener.addEventListener("click", () => {
   // links.classList.add("open-menu")
   links.classList.contains("open-menu")
@@ -9,3 +8,42 @@ opener.addEventListener("click", () => {
     : links.classList.add("open-menu");
 });
 
+var tl = gsap.timeline();
+
+tl.from("nav", {
+  y: -200,
+  opacity: 0,
+  duration: 0.4,
+});
+tl.from("nav .links ul li", {
+  y: -200,
+  opacity: 0,
+  duration: 0.4,
+  stagger:0.1
+});
+
+
+
+
+tl.from('.container-1 .heading , .container-1 .para , .container-1 .btn ',{
+  x:"-50%",
+  opacity:0, 
+  
+})
+tl.to('.container-1 .heading , .container-1 .para , .container-1 .btn ',{
+  x:0,
+  opacity:1,
+  duration:0.6, 
+})
+
+tl.from('.container-1 .image-flex ',{
+  x:"50%",
+  opacity:0, 
+  
+})
+tl.to('.container-1 .image-flex ',{
+  x:0,
+  opacity:1,
+  duration:1, 
+})
+ 
